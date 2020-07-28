8837deb1e543d280758cb8cebb313d4b48c0d012
Alan Coomes <acoomes1@cord.edu>
2020-07-28 21:19:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,21 +1,3 @@
-
-
-def countdown(x)
-  while x>0
-  puts "#{x} SECOND(S)!"
-  sleep(1)
-  x -= 1
-end
-puts "HAPPY NEW YEAR!"
-end
-
-
-
-def countdown_with_sleep(x)
-  while x>0
-  puts "#{x} SECOND(S)!"
-  sleep(1)
-  x -= 1
-end
- puts "HAPPY NEW YEAR!"
-end
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)"
